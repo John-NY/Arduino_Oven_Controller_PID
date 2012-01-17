1282b42b88e15c5423569e2b0f022ec8655080c3
John D <donovan.csulb@gmail.com>
2012-01-17 13:56:48 -0800

    first commit

---
+++ parameters.h
@@ -0,0 +1,14 @@
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+#define RELAY 7
+#define minimumRelayTime 750 // don't waste throws of the switch
+#define oneminute 60000 // 60,000 millis per minute
+#define COMMAND_SIZE 128 //our command string length
+
+#define TMIN 0 // min temp (C)
+#define TMAX 300 // max temp (C)
+#define RMIN 1 // min temp (C)
+#define RMAX 120 // max temp (C)
+
+#endif
